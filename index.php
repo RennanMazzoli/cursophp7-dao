d139b99904902360cab97c45653e9a0b38083bb3
Rennan Mazzoli <rennanyago@gmail.com>
2019-03-26 18:00:02 -0300

    criamos a classe Usuario

--- index.php
+++ index.php
@@ -2,12 +2,25 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega um usuário
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
 
-echo $root;
+//Carrega uma lista de usuários
+//$lista =  Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuários buscando pelo login
+//search = Usuario::search("jo");
+//echo json_encode($search);
 
 
+//Carrega um usuário usando o login e a senha 
+$usuario = new Usuario();
+$usuario ->Login("jose","1234567890");
+
+ECHO $usuario;
 
  ?>
